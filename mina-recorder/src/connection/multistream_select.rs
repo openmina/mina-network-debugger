90f44061a5c3e58747017a6ff17f0b11a4c5dace
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2022-07-27 18:14:10 +0300

    fix error in multistream_select

--- mina-recorder/src/connection/multistream_select.rs
+++ mina-recorder/src/connection/multistream_select.rs
@@ -6,7 +6,8 @@ use super::{DirectedId, HandleData, Cx};
 pub struct State<Inner> {
     incoming_done: bool,
     outgoing_done: bool,
-    accumulator: Vec<u8>,
+    accumulator_incoming: Vec<u8>,
+    accumulator_outgoing: Vec<u8>,
     inner: Inner,
 }
 
@@ -70,9 +71,14 @@ where
     type Output = Output<<Inner::Output as IntoIterator>::IntoIter>;
 
     fn on_data(&mut self, id: DirectedId, bytes: &mut [u8], cx: &mut Cx) -> Self::Output {
+        let accumulator = if id.incoming {
+            &mut self.accumulator_incoming
+        } else {
+            &mut self.accumulator_outgoing
+        };
         if (id.incoming && !self.incoming_done) || (!id.incoming && !self.outgoing_done) {
-            self.accumulator.extend_from_slice(bytes);
-            let cursor = &mut self.accumulator.as_slice();
+            accumulator.extend_from_slice(bytes);
+            let cursor = &mut accumulator.as_slice();
             let mut protocol = None;
             while let Some(msg) = take_msg(cursor) {
                 if let Ok(s) = std::str::from_utf8(msg) {
@@ -92,23 +98,44 @@ where
                     }
                     break;
                 } else {
-                    log::error!(" .  unparsed message: {}", hex::encode(msg));
+                    panic!(" .  unparsed message: {}", hex::encode(msg));
                 }
             }
-            self.accumulator = (*cursor).to_vec();
+            *accumulator = (*cursor).to_vec();
             match protocol {
                 None => Output::Nothing,
                 Some(p) => Output::Protocol(p),
             }
         } else {
-            let inner_out = if self.accumulator.is_empty() {
+            let inner_out = if accumulator.is_empty() {
                 self.inner.on_data(id, bytes, cx)
             } else {
-                let mut total = mem::take(&mut self.accumulator);
+                let mut total = mem::take(accumulator);
                 total.extend_from_slice(bytes);
                 self.inner.on_data(id, &mut total, cx)
             };
             Output::Inner(inner_out.into_iter())
         }
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn simple() {
+    use crate::{DirectedId, EventMetadata};
+
+    let bytes_1 = hex::decode("132f6d756c746973747265616d2f312e302e300a").unwrap();
+    let bytes_2 = hex::decode("132f6d756c746973747265616d2f312e302e300a102f636f64612f6b61642f312e302e300a2c0804122600240801122059458f97a855040a767e890855941fb130dfa3fd5a9c8213bd73d716c2e697e15001").unwrap();
+    let mut state = State::<()>::default();
+    let mut id = DirectedId {
+        metadata: EventMetadata::fake(),
+        incoming: true,
+    };
+    for item in state.on_data(id.clone(), &mut bytes_2.clone(), &mut Cx::default()) {
+        println!("{item}");
+    }
+    id.incoming = !id.incoming;
+    for item in state.on_data(id.clone(), &mut bytes_1.clone(), &mut Cx::default()) {
+        println!("{item}");
     }
 }
