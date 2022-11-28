d2f9a9c954fa11790daf84453409a65e48ef875a
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2022-11-28 14:15:33 +0000

    fix run script

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sudo rm -Rf /tmp/mina-debugger-db-$1 &> /dev/null
 nohup sudo \
     RUST_LOG='info' \
     SERVER_PORT='443' \
-    HTTPS_KEY_PATH='/etc/letsencrypt/live/debug.dev.tezedge.com/privkey.pem' \
-    HTTPS_CERT_PATH='/etc/letsencrypt/live/debug.dev.tezedge.com/fullchain.pem' \
+    HTTPS_KEY_PATH='/etc/letsencrypt/live/debug.dev.openmina.com/privkey.pem' \
+    HTTPS_CERT_PATH='/etc/letsencrypt/live/debug.dev.openmina.com/fullchain.pem' \
     DB_PATH="/tmp/mina-debugger-db-$1" \
     ~/.cargo/bin/bpf-recorder &> ~/mina-log/$1.log &
