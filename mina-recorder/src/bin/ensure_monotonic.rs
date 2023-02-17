336ede5c924ea31fd7767e560896230d94fa7de1
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2023-02-17 11:07:06 +0100

    improve local tool

--- mina-recorder/src/bin/ensure_monotonic.rs
+++ mina-recorder/src/bin/ensure_monotonic.rs
@@ -1,6 +1,6 @@
 use std::time::{SystemTime, Duration};
 
-use mina_recorder::database::FullMessage;
+use mina_recorder::database::{FullMessage, ConnectionId};
 
 use reqwest::blocking::Client;
 
@@ -8,7 +8,7 @@ fn main() {
     let client = Client::builder().build().unwrap();
     let url = "http://1.k8.openmina.com:30675/messages?limit=1000";
     let mut id = 0;
-    let mut last = None::<SystemTime>;
+    let mut last = None::<(SystemTime, ConnectionId, bool)>;
     let mut max_difference = Duration::ZERO;
     loop {
         let response = client.get(format!("{url}&id={id}")).send().unwrap();
@@ -20,16 +20,22 @@ fn main() {
             break;
         }
         for (id, msg) in &page {
-            if let Some(last) = &last {
-                if msg.timestamp < *last {
-                    let diff = last.duration_since(msg.timestamp).unwrap();
+            if let Some((last_ts, last_cn, last_incoming)) = &last {
+                if msg.timestamp < *last_ts {
+                    let diff = last_ts.duration_since(msg.timestamp).unwrap();
                     if diff > max_difference {
                         max_difference = diff;
                     }
-                    println!("message id: {id}, difference: {diff:?}");
+                    println!(
+                        "message id: {id}, difference: {diff:?}, last ({} {}), this ({} {})",
+                        last_cn, last_incoming, msg.connection_id, msg.incoming,
+                    );
+                    if *last_cn == msg.connection_id && *last_incoming == msg.incoming {
+                        println!("error, unordered message from the same connection");
+                    }
                 }
             }
-            last = Some(msg.timestamp);
+            last = Some((msg.timestamp, msg.connection_id, msg.incoming));
         }
 
         id += page.len();
